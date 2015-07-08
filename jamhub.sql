86b6ff2c2f4ba477c32627e03710b3770c4f5756
Mostafa Ghoneimy <mghoneimy@yubb-software.com>
2015-07-08 15:21:07 +0200

    database update

--- jamhub.sql
+++ jamhub.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 07, 2015 at 04:30 PM
+-- Generation Time: Jul 08, 2015 at 03:20 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -31,7 +31,7 @@ USE `jamhub`;
 DROP TABLE IF EXISTS `tracks`;
 CREATE TABLE IF NOT EXISTS `tracks` (
 `track_id` int(11) NOT NULL,
-  `name` varchar(35) NOT NULL,
+  `track_name` varchar(35) NOT NULL,
   `band` tinyint(1) DEFAULT '0',
   `user_id` int(11) DEFAULT NULL,
   `band_id` int(11) DEFAULT NULL,
@@ -44,16 +44,18 @@ CREATE TABLE IF NOT EXISTS `tracks` (
   `tags` text NOT NULL,
   `img_url` text NOT NULL,
   `track_url` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tracks`
 --
 
-INSERT INTO `tracks` (`track_id`, `name`, `band`, `user_id`, `band_id`, `duration`, `ancestor_id`, `upload_date`, `instrument`, `likes`, `rating`, `tags`, `img_url`, `track_url`) VALUES
-(1, 'track', 0, 3, NULL, 24, NULL, '0000-00-00', 'guitar', 0, 0, 'fun test', 'http:/ahourl', 'http:/wahokamanwa7ed'),
-(2, 'solotest2', 0, 3, NULL, 24, NULL, '0000-00-00', 'shit', 0, 0, 'lala', 'http:/ahourl', 'http:/wahokamanwa7ed'),
-(3, '', 0, NULL, 4, 24, NULL, '0000-00-00', '', 0, 0, '', 'http:/ahourl', 'http:/wahokamanwa7ed');
+INSERT INTO `tracks` (`track_id`, `track_name`, `band`, `user_id`, `band_id`, `duration`, `ancestor_id`, `upload_date`, `instrument`, `likes`, `rating`, `tags`, `img_url`, `track_url`) VALUES
+(1, 'track', 0, 3, NULL, 24, NULL, '0000-00-00', 'guitar', 10, 0, 'fun test', 'http:/ahourl', 'http:/wahokamanwa7ed'),
+(2, 'solotest2', 0, 3, NULL, 24, NULL, '0000-00-00', 'shit', 3, 0, 'lala', 'http:/ahourl', 'http:/wahokamanwa7ed'),
+(3, 'bandtrack', 0, NULL, 4, 24, NULL, '0000-00-00', '', 9, 0, '', 'http:/ahourl', 'http:/wahokamanwa7ed'),
+(4, 'ticktrack', 0, 3, NULL, 24, NULL, '0000-00-00', '', 20, 0, '', 'http:/ahourl', 'http:/wahokamanwa7ed'),
+(5, 'testupload', 0, 3, NULL, 24, NULL, '0000-00-00', 'testinst', 15, 0, 'upload', 'http:/ahourl', 'http:/wahokamanwa7ed');
 
 -- --------------------------------------------------------
 
@@ -68,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` text NOT NULL,
   `first_name` text,
   `last_name` text
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -101,12 +103,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `tracks`
 --
 ALTER TABLE `tracks`
-MODIFY `track_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `track_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
